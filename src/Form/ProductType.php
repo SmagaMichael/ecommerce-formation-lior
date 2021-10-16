0da2da137f1990ac337cb0d818e5c2e0411bb39d
Smaga Michaele <smaga.michael@bbox.fr>
2021-10-16 19:40:26 +0200

    gerer les données du prix avec les evenements

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -12,6 +12,8 @@ use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 
 class ProductType extends AbstractType
 {
@@ -51,7 +53,49 @@ class ProductType extends AbstractType
                 'choice_label' => function (Category $category) {
                     return strtoupper($category->getName());
                 }
-            ]);
+            ])
+
+            ;
+
+            // permet de remettre en centime dans la bdd avec ce que l'utilisateur a passé comme prix  
+            // $builder->addEventListener(FormEvents::POST_SUBMIT, function(FormEvent $event){
+            //     $product = $event->getData();
+
+            //     if($product->getPrice()!== null){
+            //         $product->setPrice($product->getPrice() * 100);
+            //     }
+            // });
+
+
+            // permet a l'utilisateur de mettre un prix en euro et pas en centime afin de ne pas calculer soit meme 
+            // $builder->addEventListener(FormEvents::PRE_SET_DATA, function(FormEvent $event){
+
+            //     $form = $event->getForm();
+
+            //     /** @var Product */
+            //     $product = $event->getData();
+
+            //     if($product->getPrice()!== null){
+            //         $product->setPrice($product->getPrice() / 100);
+            //     }
+
+
+
+
+                // SI ON VEUT MASQUER UN CHAMP SI ON EST SUR EDIT  ET LE LAISSER SUR CREATE :
+
+                // if($product->getId() === null){
+                //     $form->add('category', EntityType::class, [
+                //         'label' => 'Catégorie',
+                //         'placeholder' => '-- Choisir une catégorie --',
+                //         'class' => Category::class,
+                //         'choice_label' => function (Category $category) {
+                //             return strtoupper($category->getName());
+                //         }
+                //     ]);
+                // }
+            
+    //         });
     }
 
     public function configureOptions(OptionsResolver $resolver): void
