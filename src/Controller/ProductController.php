a8635dc15ffa212792420130a069c73741e6c8f5
Smaga Michaele <smaga.michael@bbox.fr>
2021-10-16 17:44:29 +0200

    edite d'un produit

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -26,13 +26,13 @@ use Symfony\Component\String\Slugger\SluggerInterface;
 
 class ProductController extends AbstractController
 {
+
+    
     #[Route('/{slug}', name: 'product_category')]
     public function category($slug, CategoryRepository $categoryRepository)
     {
-
         $category = $categoryRepository->findOneBy([
             'slug' => $slug
-
         ]);
 
         if (!$category) {
@@ -51,8 +51,6 @@ class ProductController extends AbstractController
     #[Route('/{category_slug}/{slug}', name: 'product_show')]
     public function show($slug, ProductRepository $productRepository)
     {
-
-
         $product = $productRepository->findOneBy([
             'slug' => $slug
         ]);
@@ -63,31 +61,53 @@ class ProductController extends AbstractController
 
         return $this->render('product/show.html.twig', [
             'product' => $product,
-
-
         ]);
     }
+
+
+
+
+
 
     #[Route('/admin/product/create', name: 'product_create')]
     public function create(Request $request,SluggerInterface $slugger, EntityManagerInterface $em)
     {
-
-        $form = $this->createForm(ProductType::class);
+        $product = new Product;
+        $form = $this->createForm(ProductType::class, $product);
 
         $form->handleRequest($request);
         if($form->isSubmitted()){
-            $product = $form->getData();
             $product->setSlug(strtolower($slugger->slug($product->getName())));
-
             $em ->persist($product);
             $em->flush();
-
         }
 
         $formView = $form->createView();
 
-
         return $this->render('product/create.html.twig', [
+            'formView' => $formView
+        ]);
+    }
+
+
+
+
+    #[Route('/admin/product/{id}/edit', name: 'product_edit')]
+    public function edit($id,ProductRepository $productRepository, Request $request, EntityManagerInterface $em)
+    {
+        $product = $productRepository->find($id);
+        $form = $this->createForm(ProductType::class,$product);
+
+
+        $form->handleRequest($request);
+        if($form->isSubmitted()){
+            $em->flush();
+        }
+
+        $formView = $form->createView();
+
+        return $this->render('product/edit.html.twig', [
+            'product' => $product,
             'formView' => $formView
         ]);
     }
