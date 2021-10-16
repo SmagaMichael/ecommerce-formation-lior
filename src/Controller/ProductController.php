57233431c296b1c62a1104b1ca4768e037d478ef
Smaga Michaele <smaga.michael@bbox.fr>
2021-10-16 03:34:04 +0200

    ajout de la page d'un produit

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\CategoryRepository;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -28,4 +29,26 @@ class ProductController extends AbstractController
             'category' => $category
         ]);
     }
+
+
+
+
+    #[Route('/{category_slug}/{slug}', name: 'product_show')]
+    public function show($slug, ProductRepository $productRepository){
+        $product = $productRepository->findOneBy([
+            'slug' => $slug
+
+        ]);
+
+        if(!$product){
+            throw new NotFoundHttpException("le produit demandé n'existe pas !");
+        }
+
+        return $this->render('product/show.html.twig', [
+            'product' => $product,
+           
+        ]);
+    }
+
+
 }
