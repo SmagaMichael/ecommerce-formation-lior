17517ffe8cad60ecd44c50e32cd9f3e2086e799b
Smaga Michaele <smaga.michael@bbox.fr>
2021-10-30 16:27:15 +0200

    factorisation de cartservice

--- src/Cart/CartService.php
+++ src/Cart/CartService.php
@@ -17,25 +17,32 @@ class CartService
         $this->productRepository = $productRepository;
     }
 
+    protected function getCart() : array{
+        return $this->session->get('cart', []);
+    }
+
+    protected function saveCart(array $cart){
+        $this->session->set('cart', $cart);
+    }
+
     public function add(int $id)
     {
 
         // 1. Retrouver le panier dans la session sous forme de tableau
         // 2. Si il n'existe pas encore, alors prendre un tableau vide
-        $cart = $this->session->get('cart', []);
+        $cart = $this->getCart();
 
         // 3.  Voir si le produit ($id) existe déjà dans le tableau
         // 4. Si c'est le cas, simplement augmenter la quantité
         // 5. Sinon, ajouter le produit avec la quantité 1
-        if (array_key_exists($id, $cart)) {
-            $cart[$id]++;
-        } else {
-            $cart[$id] = 1;
-        }
+        if (!array_key_exists($id, $cart)) {
+            $cart[$id] = 0;
+        } 
 
+        $cart[$id]++;
 
         // 6. Enregistrer le tableau mis à jour dans la session
-        $this->session->set('cart', $cart);
+        $this->saveCart($cart);
         // $request->getSession()->remove('cart');
     }
 
@@ -44,7 +51,7 @@ class CartService
     public function getTotal(): int
     {
         $total = 0;
-        foreach ($this->session->get('cart', []) as $id => $qty) {
+        foreach ($this->getCart() as $id => $qty) {
             $product =  $this->productRepository->find($id);
 
             if (!$product) {
@@ -62,32 +69,34 @@ class CartService
     {
         $detailedCart = [];
         //ce qu'on veut : [12 => ['product' => ..., 'quantity' => qté]]
-
-
-
-        foreach ($this->session->get('cart', []) as $id => $qty) {
+        foreach ($this->getCart() as $id => $qty) {
             $product =  $this->productRepository->find($id);
 
             if (!$product) {
                 continue;
             }
-
             $detailedCart[] = new CartItem($product, $qty);
         }
-
         return $detailedCart;
     }
 
+
+
+
     public function remove(int $id){
-        $cart = $this->session->get('cart', []);
+        $cart = $this->getCart();
 
         unset($cart[$id]);
 
-        $this->session->set('cart', $cart);
+        $this->saveCart($cart);
     }
 
+
+
+
+
     public function decrement(int $id){
-        $cart = $this->session->get('cart', []);
+        $cart = $this->getCart();
 
         if (!array_key_exists($id, $cart)) {
             return;
@@ -101,7 +110,7 @@ class CartService
 
         //soit le produit est a + de 1 et il faut le décrémenté
         $cart[$id]--;
-        $this->session->set('cart', $cart);
+        $this->saveCart($cart);
 
     }
 
